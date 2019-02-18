13dca73a4413b3be44f9389fb0110ff9cbf56231
lucasoskorep <lucas.oskorep@gmail.com>
2019-02-18 15:55:59 -0600

    Adding in the finished code for get and post requests.

--- app/src/main/java/com/bts/lucasoskorep/mywebrequestsapp/MainActivity.java
+++ app/src/main/java/com/bts/lucasoskorep/mywebrequestsapp/MainActivity.java
@@ -5,20 +5,136 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private static final String TAG = "MainActivity";
+    private static final String ENDPOINT_URL = "http://10.77.76.130:8080/";
+    private static final String GET_ENDPOINT = "";
+    private static final String POST_ENDOINT = "";
 
+    private RequestQueue requestQueue;
+    private TextView textView;
+    private Button getButton;
+    private Button postButton;
+    private FloatingActionButton fab;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        
+        requestQueue = Volley.newRequestQueue(this);
+        textView = findViewById(R.id.text_view_id);
+        getButton = findViewById(R.id.get_req_button);
+        postButton = findViewById(R.id.post_req_button);
+        
+        fab = findViewById(R.id.fab);
+        setFabOnClickListener();
+        setGetButtonOnClickListener();
+        setPostButtonOnClickListener();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+    private void setGetButtonOnClickListener(){
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Set the text to display ui info
+                textView.setText("Sending Get Request....Awaiting Response");
+                //Send the request
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        ENDPOINT_URL + GET_ENDPOINT,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.i(TAG, response.toString());
+                                textView.setText(response.toString());
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.i(TAG, error.getMessage());
+                                textView.setText(error.getMessage());
+                            }
+                        }
+
+                );
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+    }
+
+    /**
+     * Sets the onclick listener for the postRequest button
+     */
+    private void setPostButtonOnClickListener(){
+        //Create a new jsonObject
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("Key", "Value");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Set the button's onclick listener.
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Update the UI so that you know the request is sending
+                textView.setText("Sending Post Request....Awaiting Response");
+                //Send the request
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        ENDPOINT_URL + POST_ENDOINT,
+                        jsonObject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.i(TAG, response.toString());
+                                textView.setText(response.toString());
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.i(TAG, error.getMessage());
+                                textView.setText(error.getMessage());
+                            }
+                        }
+
+                );
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+
+
+    }
+
+
+    private void setFabOnClickListener(){
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
